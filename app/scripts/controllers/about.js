9f9cabe479d4e36aea1ef5efe0c9784718f28ca3
John D. Storey <johndangerstorey@gmail.com>
2015-09-17 22:24:48 -0600

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clockularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the clockularApp
+ */
+angular.module('clockularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clockularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the clockularApp
+ */
+angular.module('clockularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
